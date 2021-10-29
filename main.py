c20619d78213d6e6d64880f08bbc405e68a22428
ChristineWasike <christinewasike22@gmail.com>
2021-10-29 12:55:40 +0300

    Updated function shell

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def sock_merchant(n, arr):
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    nums = 10
+    sock_arr = [1, 2, 1, 3, 4, 2, 5, 4, 1, 3]
+    sock_merchant(nums, sock_arr)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
